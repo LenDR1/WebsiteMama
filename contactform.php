f0fabb583cc9793c8f111973fafbd8049a4178ee
LenDR1 <lenneth.00@hotmail.com>
2020-09-25 10:18:35 +0200

    Contactfrom Test

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['submit']){
+    $name = $_POST['fullname'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "lenneth.00@hotmail.com";
+    $headers  "From: ".$mailFrom;
+    $txt = "You have received an e-mail from".$name.".\n\n".$message;
+
+    mail($mailTo, "Contact from Website", $txt, $headers);
+    header("Location: contact.html?mailsend");
+}
